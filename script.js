20465b94421a15d7ba70d4c23294409fc050bf24
hdm6377 <158762646+hdm6377@users.noreply.github.com>
2024-02-17 19:41:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 // Variable pour vérifier si l'utilisateur a entré quelque chose
-var hasEnteredInput = false;
 var flag = "JeSuisLeFlag";
-
-// Fonction pour vérifier si l'utilisateur a entré quelque chose
-function checkInput() {
-  var userInput = document.getElementById("inputCode").value;
-  hasEnteredInput = userInput.trim() !== "";
-}
 
 // Fonction pour exécuter le code saisi par l'utilisateur
 function executeCode() {
-  if (hasEnteredInput && userInput != "alert(flag);" || userInput != "alert(flag)" {
+  var userInput = document.getElementById("inputCode").value;
+  
+  // Vérifie si l'utilisateur a entré quelque chose
+  if (userInput.trim() !== "" && userInput !== "alert(flag)") {
     alert("javascript ?");
   }
-
-  var userInput = document.getElementById("inputCode").value;
+  
   try {
     eval(userInput); // Exécute le code JavaScript entré par l'utilisateur
   } catch (error) {
